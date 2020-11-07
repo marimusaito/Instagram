e8df373c22f8c0ccc6be20ddd450704760949ecf
mari.saito <mari.saito.tec@gmail.com>
2020-11-07 22:30:07 +0900

    Firebaseとpodインストール完了

---
+++ Instagram/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  Instagram
+//
+//  Created by 齋藤万璃夢 on 2020/11/03.
+//  Copyright © 2020 mari.saito. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/UITabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  UITabBarController.swift
+//  Instagram
+//
+//  Created by 齋藤万璃夢 on 2020/11/05.
+//  Copyright © 2020 mari.saito. All rights reserved.
+//
+
+import UIKit
+
+class UITabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
