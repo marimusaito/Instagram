4115f1741de278e974c2af4f4dea22448c4f84f4
mari.saito <mari.saito.tec@gmail.com>
2020-11-25 15:47:33 +0900

    チャプター9まで完了

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 齋藤万璃夢 on 2020/11/17.
+//  Copyright © 2020 mari.saito. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
